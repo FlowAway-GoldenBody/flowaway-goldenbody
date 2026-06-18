760e6a6e79a4fdfb6f39d61aa2fd03760d0507bb
FlowAway GoldenBody <a1462978843@outlook.com>
2026-06-18 15:35:30 -0400

    same as the termux branch i messed up the order

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const os = require('os');
 const RammerheadJSMemCache = require('./classes/RammerheadJSMemCache.js');
 const RammerheadJSFileCache = require('./classes/RammerheadJSFileCache.js');
 
-const enableWorkers = false; // os.cpus().length !== 1; phones dont support workers well
+const enableWorkers = os.cpus().length; // os.cpus().length !== 1; phones dont support workers well
 
 module.exports = {
     //// HOSTING CONFIGURATION ////
