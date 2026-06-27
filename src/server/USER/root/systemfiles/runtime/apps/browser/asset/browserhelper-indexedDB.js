f4049cf3c8deab314f9cc1aa890ad6dde7acb851
FlowAway GoldenBody <a1462978843@outlook.com>
2026-06-26 20:51:43 -0400

    ...

--- src/server/USER/root/systemfiles/runtime/apps/browser/asset/browserhelper-indexedDB.js
+++ src/server/USER/root/systemfiles/runtime/apps/browser/asset/browserhelper-indexedDB.js
@@ -1,2 +1,15 @@
-window.browserGlobals.clearIndexedDbForSite = async function (siteOrigin) {};
-window.browserGlobals.updateIndexedDbStore = async function (profileName) {};
+window.browserGlobals.clearIndexedDbForSite = async function (siteOrigin) {
+    window.protectedGlobals.DeleteFile(`/systemfiles/runtime/apps/browser/${window.browserGlobals.getCurProfileName()}/localstorage/indexedDB/${siteOrigin}.json`);
+};
+window.browserGlobals.updateIndexedDbStore = async function (profileName) {
+    let directory = await window.protectedGlobals.ReadFolder(`/systemfiles/runtime/apps/browser/${profileName}/localstorage/indexedDB`);
+    if (!directory) {
+        await window.protectedGlobals.WriteFolder(`/systemfiles/runtime/apps/browser/${profileName}/localstorage/indexedDB`);
+    }
+    for (const browser of window.browserGlobals.allBrowsers) {
+        for (const tab of browser.tabs) {
+            const siteOrigin = window.browserGlobals.mainWebsite(tab.iframe.contentWindow.location.href);
+            // ...
+        }
+    }
+};
