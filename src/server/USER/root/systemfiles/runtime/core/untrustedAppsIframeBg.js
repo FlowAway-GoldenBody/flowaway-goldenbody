56f9b3632cb17386232783dd94c46e1b6651bf6a
FlowAway GoldenBody <a1462978843@outlook.com>
2026-07-08 14:21:28 -0400

    .

--- src/server/USER/root/systemfiles/runtime/core/untrustedAppsIframeBg.js
+++ src/server/USER/root/systemfiles/runtime/core/untrustedAppsIframeBg.js
@@ -22,8 +22,9 @@
             });
             window.dispatchEvent(clickEvent);
             document.dispatchEvent(clickEvent);
-        } else if (event.data.keydownOnApp) {
-            const keyEvent = new KeyboardEvent("keydown", {
+        } else if (event.data.keydownOnApp || event.data.keyupOnApp) {
+            const type = event.data.keydownOnApp ? "keydown" : "keyup";
+            const keyEvent = new KeyboardEvent(type, {
                 key: event.data.key || event.data.comboKey || "",
                 code: event.data.code || "",
                 ctrlKey: !!event.data.ctrl,
@@ -33,11 +34,31 @@
                 repeat: !!event.data.repeat,
                 bubbles: true,
                 cancelable: true,
+                composed: true,
             });
             keyEvent.__gbComboKey = event.data.comboKey || "";
             keyEvent.__gbOriginalKey = event.data.key || "";
             window.dispatchEvent(keyEvent);
             document.dispatchEvent(keyEvent);
+            if (document.body) document.body.dispatchEvent(keyEvent);
+        } else if (event.data.pointerdownOnApp || event.data.pointerupOnApp) {
+            const type = event.data.pointerdownOnApp ? "pointerdown" : "pointerup";
+            const mouseEvent = new MouseEvent(type, {
+                bubbles: true,
+                cancelable: true,
+                view: window,
+                button: typeof event.data.button === "number" ? event.data.button : 0,
+                clientX: typeof event.data.clientX === "number" ? event.data.clientX : 0,
+                clientY: typeof event.data.clientY === "number" ? event.data.clientY : 0,
+                ctrlKey: !!event.data.ctrl,
+                altKey: !!event.data.alt,
+                shiftKey: !!event.data.shift,
+                metaKey: !!event.data.meta,
+                composed: true,
+            });
+            window.dispatchEvent(mouseEvent);
+            document.dispatchEvent(mouseEvent);
+            if (document.body) document.body.dispatchEvent(mouseEvent);
         }
     });
 })();
--- src/server/USER/root/systemfiles/runtime/core/untrustedIframePatch.js
+++ src/server/USER/root/systemfiles/runtime/core/untrustedIframePatch.js
@@ -29,6 +29,7 @@ window.lockAPI("localStorage", window);
 window.lockAPI("sessionStorage", window);
 window.lockAPI("caches", window);
 window.lockAPI("cookies", document);
+
 Object.defineProperty(window.HTMLInputElement.prototype, 'type', {
     set: (val) => {if (val == 'file') return new Error("Access to file input is Banned.");},
     configurable: false
@@ -187,6 +188,11 @@ window.addEventListener("keydown", (e) => {
     const key = e.key === " " ? "Space" : e.key;
     const comboKey = parts.length ? parts.concat(String(key)).join("+") : String(key);
 
+    if (e.key === "Tab" && (e.altKey || e.ctrlKey || e.metaKey)) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
     window.parent.postMessage({
         keydownOnApp: true,
         key: key,
@@ -199,3 +205,67 @@ window.addEventListener("keydown", (e) => {
         repeat: e.repeat,
     }, '*');
 });
+window.addEventListener("keyup", (e) => {
+    const parts = [];
+    if (e.ctrlKey) parts.push("Ctrl");
+    if (e.altKey) parts.push("Alt");
+    if (e.metaKey) parts.push("Meta");
+    if (e.shiftKey) parts.push("Shift");
+    const key = e.key === " " ? "Space" : e.key;
+    const comboKey = parts.length ? parts.concat(String(key)).join("+") : String(key);
+
+    window.parent.postMessage({
+        keyupOnApp: true,
+        key: key,
+        comboKey: comboKey,
+        code: e.code,
+        ctrl: e.ctrlKey,
+        alt: e.altKey,
+        shift: e.shiftKey,
+        meta: e.metaKey,
+        repeat: e.repeat,
+    }, '*');
+});
+window.addEventListener("pointerdown", (e) => {
+    window.parent.postMessage({
+        pointerdownOnApp: true,
+        button: e.button,
+        clientX: e.clientX,
+        clientY: e.clientY,
+        ctrl: e.ctrlKey,
+        alt: e.altKey,
+        shift: e.shiftKey,
+        meta: e.metaKey,
+    }, '*');
+});
+window.addEventListener("pointerup", (e) => {
+    window.parent.postMessage({
+        pointerupOnApp: true,
+        button: e.button,
+        clientX: e.clientX,
+        clientY: e.clientY,
+        ctrl: e.ctrlKey,
+        alt: e.altKey,
+        shift: e.shiftKey,
+        meta: e.metaKey,
+    }, '*');
+});
+document.createElement =  function (...args) {
+    const element = HTMLDocument.prototype.createElement.apply(this, args);
+    if (args[0] === "iframe") {
+        throw new Error("Access to iframe creation is Banned.");
+    }
+    Object.defineProperty(element, "attachShadow", {
+        get: function () {
+            throw new Error("Access to attachShadow is Banned.");
+        },
+        set: function () {
+            throw new Error("Access to attachShadow is Banned.");
+        },
+        configurable: false,
+    });
+    element.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+    });
+    return element;
+};
